86ca3c02ad52814d022abb99fcdcaf93f554937b
faris <faris38kanbur@gmail.com>
2020-12-26 18:21:27 +0100

    add actions types

---
+++ mytodo/src/ActionTypes.js
@@ -0,0 +1,5 @@
+export const ACTIONS = {
+    TODO_ADD: "todo_add",
+    TODO_DEL: "todo_del",
+    TODO_COMP: "todo_comp"
+};
--- mytodo/src/App.js
+++ mytodo/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useReducer} from 'react';
+import { ACTIONS } from './ActionTypes';
 
-function App() {
+
+const reducer = {state, action} = ();
+
+
+
+
+
+const App = () => {
+  const [todo, dispatch] = useReducer(reducer, []);
+  const [ name, setName] = useState("");
+
+
+  const handleSubmit = () =>{
+    dispatch({ type: ACTIONS.TODO_ADD });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={name} onChange={e = setName(e.target.value)} />
+        </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
