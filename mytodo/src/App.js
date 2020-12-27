27c58f4e994654499a286b0398c3cc9849b29ad3
faris <faris38kanbur@gmail.com>
2020-12-27 20:30:06 +0100

    add filter

--- mytodo/src/App.js
+++ mytodo/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react'
+import React, {useState, useEffect } from 'react'
 import "./App.css";
 
 import Form from "./components/Form";
@@ -7,16 +7,50 @@ import TodoList from './components/TodoList';
 
 
 const App = () => {
+
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+
+  const filterHandler = () => {
+    switch(status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
 
   return (
     <div className ="App">
       <header>
         <h1>Faris' Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos}  setInputText={setInputText}/>
-      <TodoList setTodos={setTodos} todos={todos} />
+      <Form 
+          inputText={inputText}
+          todos={todos} 
+          setTodos={setTodos} 
+          setInputText={setInputText}
+          setStatus={setStatus}
+          
+          />
+      <TodoList 
+          setTodos={setTodos} 
+          todos={todos} 
+          filteredTodos={filteredTodos}
+          />
     </div>
   )
 }
--- mytodo/src/components/Form.js
+++ mytodo/src/components/Form.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Form = ({ setInputText, todos, setTodos, inputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText,setStatus }) => {
 
     const inputTextHandler = (e) => {
         console.log(e.target.value);
         setInputText(e.target.value);
     };
+
 
     const submitTodoHandler = (e) => {
         e.preventDefault();
@@ -15,6 +16,14 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
         ]);
         setInputText("");
     };
+
+
+
+    const statusHandler = (e) => {
+        setStatus(e.target.value)
+    }
+
+
     return(
         <form>
             <input 
@@ -23,8 +32,9 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
             <button onClick={submitTodoHandler} className="todo-button" type="submit" >
                 <i className="fas fa-plus-square"></i>
             </button>
+
             <div className="select">
-                <select name="todos" className="filter-todo">
+                <select onChange={statusHandler} name="todos" className="filter-todo">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="uncompleted">Uncompleted</option>
