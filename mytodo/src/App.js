92ea202bbf166eebbabe8d1c52811eb9ea06a91e
faris <faris38kanbur@gmail.com>
2020-12-27 18:40:24 +0100

    add todo

--- mytodo/src/App.js
+++ mytodo/src/App.js
@@ -8,14 +8,15 @@ import TodoList from './components/TodoList';
 
 const App = () => {
   const [inputText, setInputText] = useState("");
-  
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className ="App">
       <header>
         <h1>Faris' Todo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form inputText={inputText} todos={todos} setTodos={setTodos}  setInputText={setInputText}/>
+      <TodoList todos={todos} />
     </div>
   )
 }
--- mytodo/src/components/TodoList.js
+++ mytodo/src/components/TodoList.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
+    console.log(todos);
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>            
+            <ul className="todo-list">
+                {
+                    todos.map((todo) =>(
+                        <Todo />
+                    ))
+                }
+               
+                
+                </ul> 
         </div>
-    )
-}
+    );
+};
 
-export default TodoList
+export default TodoList;
