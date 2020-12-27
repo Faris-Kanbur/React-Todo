c361650f0d54f13188c19665729df4af5386d389
faris <faris38kanbur@gmail.com>
2020-12-27 19:50:25 +0100

    added delete style

--- mytodo/src/components/Todo.js
+++ mytodo/src/components/Todo.js
@@ -7,10 +7,23 @@ const Todo = ({text,todo, todos, setTodos}) => {
 
     };
 
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if(item.id === todo.id){
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }))
+    }
+
+
+
     return (
         <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button className="complete-btn"> <i className="fas fa-check"></i> </button>
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            <button onClick={completeHandler} className="complete-btn"> <i className="fas fa-check"></i> </button>
             <button onClick={deleteHandler} className="trash-btn" > <i className="fas fa-trash"></i> </button>
         </div>
     );
